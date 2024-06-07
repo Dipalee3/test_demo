607fd0feaf2bdf9ec16841ab6a4542d571a9786b
Dipalee <dipaleepatil1@gmail.com>
2024-06-07 11:30:38 +0530

    Unchange file

---
+++ Database/srms.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2021 at 02:49 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `srms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_login`
+--
+
+CREATE TABLE `admin_login` (
+  `userid` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `admin_login`
+--
+
+INSERT INTO `admin_login` (`userid`, `password`) VALUES
+('admin', '123'),
+('Dipalee', 'Patil'),
+('Mamta', 'Kolhe'),
+('Megha', 'Chaudhari'),
+('Paurnima', 'Patil');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `name` varchar(30) NOT NULL,
+  `id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`name`, `id`) VALUES
+('FY', 1),
+('SY', 2),
+('TY', 3),
+('FY Comp', 4),
+('SY Comp', 6),
+('TY Ele', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `name` varchar(30) NOT NULL,
+  `rno` int(3) NOT NULL,
+  `class` varchar(30) NOT NULL,
+  `p1` int(3) NOT NULL,
+  `p2` int(3) NOT NULL,
+  `p3` int(3) NOT NULL,
+  `p4` int(3) NOT NULL,
+  `p5` int(3) NOT NULL,
+  `marks` int(3) NOT NULL,
+  `percentage` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`name`, `rno`, `class`, `p1`, `p2`, `p3`, `p4`, `p5`, `marks`, `percentage`) VALUES
+('Abhijeet ', 1, 'FY', 70, 87, 97, 80, 78, 412, 82.4),
+('Jayesh Firke', 1, 'FY Comp', 88, 89, 98, 97, 86, 458, 91.6),
+('Sonal Zope', 10, 'SY', 70, 87, 89, 90, 84, 420, 84),
+('Pritesh Patil', 2, 'SY Comp', 88, 89, 98, 78, 86, 439, 87.8),
+('Dipalee Patil', 3, 'TY', 76, 87, 83, 79, 81, 406, 81.2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `name` varchar(30) NOT NULL,
+  `rno` int(3) NOT NULL,
+  `class_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`name`, `rno`, `class_name`) VALUES
+('Abhijeet ', 1, 'FY'),
+('Rahul', 2, 'FY'),
+('Jayesh Firke', 1, 'FY Comp'),
+('Sonal Zope', 10, 'SY'),
+('Pritesh Patil', 2, 'SY Comp'),
+('Dipalee Patil', 3, 'TY'),
+('Mamata Kolhe', 5, 'TY'),
+('Paurnima Patil', 4, 'TY'),
+('Rajendra', 10, 'TY Ele');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`class`),
+  ADD KEY `class` (`class`),
+  ADD KEY `name` (`name`,`rno`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`name`,`rno`),
+  ADD KEY `class_name` (`class_name`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`class`) REFERENCES `class` (`name`),
+  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`name`,`rno`) REFERENCES `students` (`name`, `rno`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class_name`) REFERENCES `class` (`name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
